c20afce9d6ec8d4b904668129f9f3e6f834de751
GYANAM <ggyanam869@gmail.com>
2022-05-30 09:41:58 +0530

    Fault updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,11 @@
 export default function Footer() {
   return (
     <>
-      <center>
-        <div className="mb-10 text-white">
-          <p>Animeflix 2022. All rights reserved.</p>
-          <p>Developed by Ayush and Gyanam</p>
-          <p>Contact: contactus@animeflixed.com</p>
-        </div>
-      </center>
+      <div className="mb-10 items-center text-white">
+        <p>Animeflix 2022. All rights reserved.</p>
+        <p>Developed by Ayush and Gyanam</p>
+        <p>Contact: contactus@animeflixed.com</p>
+      </div>
     </>
   );
 }
