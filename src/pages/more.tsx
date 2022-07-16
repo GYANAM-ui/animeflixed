2b558d1d40771c70747824a4ca2371ac66d284c1
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2022-07-16 13:30:13 +0000

    ghtfghftgh

--- src/pages/more.tsx
+++ src/pages/more.tsx
@@ -1,9 +1,28 @@
+import Link from 'next/link';
+
 import Header from '@components/Header';
 
 const MoreNav: React.FC<{}> = () => {
   return (
     <>
-     <Header />
+      <Header />
+        <div className="text-align-center">
+          <Link href="/">
+              <a href="">Home</a>
+          </Link>
+          <hr>
+          <Link href="/upgrade">
+              <a href="">Upgrade</a>
+          </Link>
+          <hr>
+          <Link href="/latst-fearures">
+              <a href="">Latest</a>
+          </Link>
+          <hr>
+          <Link href="/">
+              <a href="#about">About</a>
+          </Link>
+        </div>
     </>
   );
 };
